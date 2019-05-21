15c9f9c1db157c8febc5996e4d681a55e08ffc7c
Grayson Stanton <gstanton@unomaha.edu>
2019-05-21 13:51:23 -0500

    Initial Commit

---
+++ MCMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCMS
+//
+//  Created by Grayson Stanton on 5/21/19.
+//  Copyright © 2019 Grayson Stanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
